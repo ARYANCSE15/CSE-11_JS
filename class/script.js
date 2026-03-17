b16c2a88d650799c11a3830f68c284e9f8697011
ARYANCSE15 <aryansharma0379@gmail.com>
2026-03-17 09:26:04 +0530

    Merge branch 'main' of https://github.com/ARYANCSE15/CSE-11_JS

--- class/script.js
+++ class/script.js
@@ -9,7 +9,7 @@ console.log(num);
 console.log(typeof(num));
 var sayHello=function(){
     console.log("Hello world")
-}
+};
 sayHello()
 var numberArray=[1,2,3];
 var animals=["cat","dog","cow"];
@@ -18,7 +18,7 @@ var person={
     age:'51',
     title:"hello",
 };
-const sym1=Symbol(3)
+const sym1=Symbol(3);
 console.log(sym1);
 const sym2=Symbol(4);
 console.log(sym2);
@@ -29,9 +29,35 @@ else{
     console.log("false")
 }
 var count;
-document.write('Starting loop'+"<br/>");
+document.write('Starting loop'+'<br/>');
 for(count=0;count<10;count++){
-    document.write("Current count:"+count);
-    document.write("<br/>");
+    document.write('Current count:'+count);
+    document.write('<br/>');
 }
 document.write("loop stopped")
+//arrow function-anonymous//
+var add2 = (a,b) =>{
+    console.log(a+b);
+};
+add2(102,30);
+let x=new Object();
+//object is the class it is key value//
+//array is an ordered collection of data//
+//array is a special kind of object//
+var myinfo=new Array();
+var myinfo1=Array();
+console.log(myinfo,myinfo1);
+let collection=[
+    {},
+    [],
+    "Aryan",
+    15,
+    undefined,
+    null,
+    new String("abc"),
+    new Date(),
+];
+console.log(collection);
+collection['Teacher Name']="John";
+collection.phoneNo=7249792742;
+console.log(collection);
---
+++ class/scriptdom.js
@@ -0,0 +1,27 @@
+const result=document.getElementById("para");
+result.innerHTML="Hello World";
+result.title="AS";
+console.log(result);
+const x=document.getElementsByClassName("sand");
+console.log(x);
+x[0].innerHTML="Aryan";
+x[1].innerHTML="Sharma";
+const result3=document.getElementsByTagName("p");
+result3[2].innerHTML="Archit is nigga";
+result3[1].style.color="blue";
+result3[1].innerText="my paragraph";
+const test2= document.querySelectorAll("ul li");
+for(x in test2){
+    test2[x].style.backgroundColor="green";
+    test2[x].style.margin="10px";
+    test2[x].style.color="blue";
+}
+function myFunction(){
+(document.getElementById("myh1").setAttribute("class","democlass"));
+alert(document.getElementById("myh1").getAttribute("class"));
+}
+
+
+
+
+
